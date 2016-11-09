4b10d63db5dfcdaaa7379d2b40c64fbae6e0941f
Eduard Urbach <e.urbach@gmail.com>
2016-11-09 22:50:44 +0900

    Minor changes

--- Fonts.go
+++ Fonts.go
@@ -3,13 +3,13 @@ package main
 import (
 	"strings"
 
-	"github.com/aerojs/aero"
+	"github.com/aerogo/aero"
 )
 
+const fontsUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+
 func getFontsCSS() string {
-	fontsCSS, err := aero.GetWithHeaders("https://fonts.googleapis.com/css?family=Ubuntu", aero.Headers{
-		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-	})
+	fontsCSS, err := aero.Get("https://fonts.googleapis.com/css?family=Ubuntu").Header("User-Agent", fontsUserAgent).Send()
 
 	if err != nil {
 		return ""
