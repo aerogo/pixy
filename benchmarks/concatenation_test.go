d39f960ec762798f2cdbdaf8c0a888df1ac1bdd0
Eduard Urbach <e.urbach@gmail.com>
2016-11-30 11:35:32 +0900

    Added buffer benchmarks

---
+++ benchmarks/concatenation_test.go
@@ -0,0 +1,161 @@
+package pixy
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+const testString = "xx"
+
+var testBytes = []byte(testString)
+
+// BenchmarkJoin ...
+func BenchmarkJoin(b *testing.B) {
+	c := make([]string, b.N)
+
+	for n := 0; n < b.N; n++ {
+		c[n] = testString
+	}
+
+	str := strings.Join(c, "")
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkJoin")
+	}
+}
+
+// BenchmarkWrite ...
+func BenchmarkWrite(b *testing.B) {
+	var buffer bytes.Buffer
+
+	for n := 0; n < b.N; n++ {
+		buffer.Write(testBytes)
+	}
+
+	str := buffer.String()
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkWrite")
+	}
+}
+
+// BenchmarkWriteString ...
+func BenchmarkWriteString(b *testing.B) {
+	var buffer bytes.Buffer
+
+	for n := 0; n < b.N; n++ {
+		buffer.WriteString(testString)
+	}
+
+	str := buffer.String()
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkWriteString")
+	}
+}
+
+// BenchmarkWriteStringKL ...
+func BenchmarkWriteStringKL(b *testing.B) {
+	var buffer bytes.Buffer
+	buffer.Grow(b.N)
+
+	for n := 0; n < b.N; n++ {
+		buffer.WriteString(testString)
+	}
+
+	str := buffer.String()
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkWriteStringKL")
+	}
+}
+
+// BenchmarkCopyUL ...
+func BenchmarkCopyUL(b *testing.B) {
+	s := make([]string, b.N)
+	_l := 0
+
+	for n := 0; n < b.N; n++ {
+		s[n] = testString
+		_l += len(testString)
+	}
+
+	_b := make([]byte, _l)
+	_c := 0
+
+	for i := 0; i < b.N; i++ {
+		_c += copy(_b[_c:], s[i])
+	}
+
+	str := string(_b)
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkCopyUL")
+	}
+}
+
+// BenchmarkCopyStringKL ...
+func BenchmarkCopyStringKL(b *testing.B) {
+	_l := len(testString) * b.N
+	_b := make([]byte, _l)
+	_c := 0
+
+	for n := 0; n < b.N; n++ {
+		_c += copy(_b[_c:], testString)
+	}
+
+	str := string(_b)
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkCopyKL")
+	}
+}
+
+// BenchmarkCopyBytesKL ...
+func BenchmarkCopyBytesKL(b *testing.B) {
+	_l := len(testString) * b.N
+	_b := make([]byte, _l)
+	_c := 0
+
+	for n := 0; n < b.N; n++ {
+		_c += copy(_b[_c:], testBytes)
+	}
+
+	str := string(_b)
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkCopyKL")
+	}
+}
+
+// BenchmarkMix ...
+func BenchmarkMix(b *testing.B) {
+	var buffer bytes.Buffer
+
+	if b.N == 1 {
+		b.N = 2
+	}
+
+	half := b.N / 2
+	_l := len(testString) * half
+	_b := make([]byte, _l)
+	_c := 0
+
+	for n := 0; n < half; n++ {
+		buffer.WriteString(testString)
+	}
+
+	for n := 0; n < half; n++ {
+		_c += copy(_b[_c:], testBytes)
+	}
+
+	buffer.Write(_b)
+
+	str := buffer.String()
+
+	if len(str) != b.N*len(testString) {
+		fmt.Println("Error BenchmarkMix")
+	}
+}
