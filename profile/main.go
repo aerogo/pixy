78f041f527ed8e470c61ff510ef6193a5764a367
Eduard Urbach <e.urbach@gmail.com>
2017-10-24 16:10:20 +0200

    Added program to profile code

---
+++ profile/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/aerogo/pixy"
+	"github.com/pkg/profile"
+)
+
+func main() {
+	defer profile.Start(profile.MemProfile).Stop()
+
+	src, _ := ioutil.ReadFile("examples/post-benchmark.pixy")
+	code := string(src)
+
+	for i := 0; i < 100000000; i++ {
+		pixy.Compile(code)
+	}
+}
